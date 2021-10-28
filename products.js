6f4c135dca02501a05b563b24edc02dc2242d92a
theerutth <theerut.thankan@mail.kmutt.ac.th>
2021-10-28 17:23:41 +0700

    edit

--- products.js
+++ products.js
@@ -1,4 +1,4 @@
-class Products {
+class Product {
     constructor (productId , productName , price , remainingAmount , image){
         this._productId = productId;
         this._productName = productName;
@@ -10,7 +10,7 @@ class Products {
         return this._productId;
     }
 
-    get prodcutName(){
+    get productName(){
         return this._productName;
     }
     
@@ -25,4 +25,16 @@ class Products {
     get image() {
         return this._image;
     }
+
 }
+
+let products = [
+    new Product('MS01','LOGITECH G PRO X SUPERLIGHT',4990.00,20,'./images/mouse.jfif') , 
+    new Product('MT01','GIGABYTE G27Q 27" IPS 2K 144Hz',11900.00,15,'./images/monitor.jpg'),
+    new Product('MC01','HYPERX SOLOCAST',1990.00,10,'./images/microphone.jpg'),
+    new Product('HP01','HEADSET ALIENWARE AW510H (LUNAR LIGHT)',3390.00,25,'./images/headphone.jfif'),
+    new Product('KB01','KEYCHRON K2 V.2',3890.00,25,'./images/keyboard.jpg'),
+    new Product('MP01','RAZER GAMING MOUSE PAD FIREFLY',1160.00,25,'./images/mousepad.jpg')
+]
+
+export{products}
