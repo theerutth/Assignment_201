e0e7fcfe66b38e8459a917e7d685e87b0b9f58d3
theerutth <theerut.thankan@mail.kmutt.ac.th>
2021-11-15 12:09:09 +0700

    editcode

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ import { products } from "./products.js";
 import { addButton } from "./cart/itemCart.js";
 
 const productList = document.getElementById('productList');
-
-
 const searchBar = document.getElementById('search-bar');
 
 searchBar.addEventListener('input', () => {
@@ -95,3 +93,35 @@ const hide = document.getElementById('hidebutton');
 hide.addEventListener("click", () => {
     hidesearch()
 });
+
+//เปลี่ยนเป็นdarkmode
+const themeSwitch = document.querySelector('input');
+themeSwitch.addEventListener('change', () => {
+
+    document.body.classList.toggle('dark-theme');
+    setCookie("darkmode");
+});
+
+//ก้อปโค้ดมา
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {
+    document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
